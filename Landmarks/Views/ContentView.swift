8139a2fc27d244cff5c284036589ad062423e340
David Aaron <davidaarontripp@gmail.com>
2021-04-29 16:55:10 -0700

    Completed Chapter 3 - Section 5: Adopt the Model Object in Your Views.

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -15,8 +15,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
             ContentView()
-        }
+                .environmentObject(ModelData())
     }
 }
