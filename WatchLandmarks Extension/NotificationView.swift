3eda85a310d26750244dc24ea3afe88fe6890887
David Aaron Tripp <davidaarontripp@gmail.com>
2021-06-02 12:53:08 -0700

    Completed Section 1 of Creating a watchOS App.

---
+++ WatchLandmarks Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  WatchLandmarks Extension
+//
+//  Created by David Aaron on 6/2/21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
