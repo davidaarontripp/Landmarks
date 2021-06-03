d0d811b46de0778fdc08bc781ba4218d4cf24482
David Aaron Tripp <davidaarontripp@gmail.com>
2021-06-03 16:57:36 -0700

    Completed Section 5 of Creating a macOS App.

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by David Aaron on 6/3/21.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
